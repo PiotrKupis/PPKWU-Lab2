41e5fcae701d19872c401c451cfd97a35577356b
Piotr Kupis <piotr.kupis42@gmail.com>
2021-10-28 12:10:40 +0200

    initial commit

---
+++ src/main/java/com/ppkwu/stringapi/StringApiApplication.java
@@ -0,0 +1,13 @@
+package com.ppkwu.stringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringApiApplication.class, args);
+    }
+
+}
