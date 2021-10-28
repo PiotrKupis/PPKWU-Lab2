826179e5dc2ace0d7ec8c22c5b0ce7aec8c3d75d
Piotr Kupis <piotr.kupis42@gmail.com>
2021-10-28 13:21:21 +0200

    add counting uppercase

---
+++ src/main/java/com/ppkwu/stringapi/StringController.java
@@ -0,0 +1,28 @@
+package com.ppkwu.stringapi;
+
+import com.google.gson.Gson;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller responsible for handling operations connected with strings.
+ */
+@RestController
+public class StringController {
+
+    @GetMapping("string/{string}/{substring}")
+    public ResponseEntity<String> countUpper(@PathVariable("string") String string,
+        @PathVariable("substring") String substring) {
+        Response response = new Response();
+
+        long uppercase = string.chars()
+            .filter(Character::isUpperCase)
+            .count();
+
+        response.setUppercase(uppercase);
+
+        return ResponseEntity.ok(new Gson().toJson(response));
+    }
+}
